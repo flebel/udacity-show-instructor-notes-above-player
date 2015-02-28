1cc6799813c0767cd749d06d2bd7a6948490ad6f
Francois Lebel <francoislebel@gmail.com>
2015-02-28 08:15:47 -0800

    Added support for quiz player.

--- instructorizer.js
+++ instructorizer.js
@@ -6,10 +6,12 @@ var rowExists = setInterval(function() {
   col = document.getElementsByClassName("col-xs-9 ng-scope")[0];
   if (col) {
     row = col.getElementsByClassName("row");
-    player = row[0];
     notes = row[2];
-    player.parentNode.insertBefore(notes, player);
-    clearInterval(rowExists);
+    // Make sure `notes` really is the instructor's notes div
+    if (notes.getElementsByClassName("col-xs-8")[0]) {
+      player = row[0];
+      player.parentNode.insertBefore(notes, player);
+    }
   }
-}, 100);
+}, 500);
 
